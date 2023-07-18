f3e0a10d72c1473aa090468e0543b80a9c7fcea7
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-18 09:08:38 +0800

    updated config file

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://j2911realty-k7b8l.ondigitalocean.app' : undefined,
+}
 
 module.exports = nextConfig
