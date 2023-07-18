0822e4eb5fc6e27b847c7b90cc454974fd20cc0b
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-18 19:18:32 +0800

    added admin.js in prod

---
+++ strapi/config/env/production/admin.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+  url: env('APP_STRAPI_ADMIN_URL')
+});
