cd79710e86c019564b766d0163266545a232e29e
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-18 19:54:35 +0800

    hardcoded url

--- strapi/config/env/production/server.js
+++ strapi/config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env('APP_STRAPI_URL'), // Sets the public URL of the application.
-  // url: "https://j2911realty-k7b8l.ondigitalocean.app/strapi",
+  // url: env('APP_STRAPI_URL'), // Sets the public URL of the application.
+  url: "https://j2911realty-k7b8l.ondigitalocean.app/strapi",
   app: {
     keys: env.array('APP_KEYS'),
   },
