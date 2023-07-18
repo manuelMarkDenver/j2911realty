e42cc07f67704d80b6c829025acb4c6f45280723
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-07-18 14:08:06 +0800

    updated app url

--- strapi/config/env/production/server.js
+++ strapi/config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("APP_URL"), // Sets the public URL of the application.
+  url: env("APP_URL_STRAPI"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
   },
