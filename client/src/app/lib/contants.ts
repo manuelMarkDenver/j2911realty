1201d6543ae978ab40ebdcd5e3088316db4224c1
manuelMarkDenver <manuel.markdenver@gmail.com>
2023-08-11 23:40:34 +0800

    updated constants variables

--- client/src/app/lib/contants.ts
+++ client/src/app/lib/contants.ts
@@ -1,27 +1,10 @@
-type ContactInfoType = {
-  type: string;
-  link?: string;
-  value?: string
+const constants = {
+  FB_ACCOUNT: "https://www.facebook.com/blhernandez04",
+  FB_PAGE: "https://www.facebook.com/profile.php?id=100091152622740",
+  IG_ACCOUNT: "https://www.instagram.com/j.2911realty/",
+  AIRBNB_ACCOUNT: "https://www.airbnb.com/users/show/318451841",
+  EMAIL: "blhernandez0923@gmail.com",
+  CONTACT_NUMBER: "774 888 8571",
 };
 
-const contact_info: ContactInfoType[] = [
-  {
-    type: "email", value: "blhernandez0923@gmail.com",
-  },
-  { type: "contact_number", value: "774 888 8571" },
-];
-
-const social_media_links: ContactInfoType[] = [
-  {
-    type: "fb_account",
-    link: "https://www.facebook.com/blhernandez04",
-  },
-  {
-    type: "fb_page",
-    link: "https://www.facebook.com/profile.php?id:100091152622740",
-  },
-  { type: "ig_acct", link: "https://www.instagram.com/j.2911realty/" },
-  { type: "air_bnb", link: "https://www.airbnb.com/users/show/318451841" },
-];
-
-export { contact_info, social_media_links };
+export default constants;
